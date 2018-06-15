1fa5f6bf4a5bfe0da747f47d5c0ad3f805c5b015
Joseph Donovan <joeydonovan4@gmail.com>
2018-06-15 12:20:14 -0400

    App folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
-import './index.css';
 import { store } from './store/index';
-import App from './App';
+import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 render(
